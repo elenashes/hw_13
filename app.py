a29fbd961d31865f2fbd406f20d54a18f97df5e0
elenashes <97842107+elenashes@users.noreply.github.com>
2022-02-06 17:32:55 +0300

    домашка к 13 уроку

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+import json
 from flask import Flask, request, render_template, send_from_directory
-# from functions import ...
+from functions import getting_all_tags, getting_post_by_tag
 
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
@@ -9,17 +10,42 @@ app = Flask(__name__)
 
 @app.route("/")
 def page_index():
-    pass
+    tag_list = getting_all_tags()
+    return render_template('index.html', tag_list=tag_list)
 
 
 @app.route("/tag")
 def page_tag():
-    pass
+    tag_name = request.args.get("tag")
+    matching_posts = getting_post_by_tag(tag_name)
+    return render_template('post_by_tag.html', posts=matching_posts, tag=tag_name)
 
 
-@app.route("/post", methods=["GET", "POST"])
+@app.route("/post", methods=["GET"])
+def page_post_form():
+    return render_template('post_form.html')
+
+
+@app.route("/post", methods=["POST"])
 def page_post_create():
-    pass
+    picture = request.files.get("picture")
+    content = request.values.get("content")
+
+    if not picture:
+        return "Ошибка загрузки"
+
+    filename = picture.filename
+    path = "./"+UPLOAD_FOLDER+"/"+filename
+    picture.save(path)
+
+    picture_url = "/"+UPLOAD_FOLDER+"/"+filename
+
+    data_dict = {"pic": picture_url, "content": content}
+
+    with open('posts.json', 'a', encoding="utf-8") as file:
+        json.dump(data_dict, file, ensure_ascii=False)
+
+    return render_template('post_uploaded.html', picture=picture_url, content=content)
 
 
 @app.route("/uploads/<path:path>")
--- functions.py
+++ functions.py
@@ -1,1 +1,35 @@
 import json
+
+def reading_posts():
+    with open('posts.json', 'r', encoding="utf-8") as file:
+        posts_data_json = json.load(file)
+    return posts_data_json
+
+def getting_all_tags():
+    posts_list = reading_posts()
+    tag_list = []
+    for post in posts_list:
+        content_words = post["content"].split(" ")
+        for word in content_words:
+            if word.startswith("#"):
+                if not "!" in word:
+                    tag_list.append(word[1:])
+                else:
+                    tag_list.append(word[1:].rstrip("!"))
+    return tag_list
+
+def getting_post_by_tag(tag_name):
+    posts_list = reading_posts()
+    matching_posts = []
+    for post in posts_list:
+        content_words = post["content"].split(" ")
+        for word in content_words:
+            if tag_name in word:
+                matching_posts.append(post)
+    return matching_posts
+
+
+
+
+
+
